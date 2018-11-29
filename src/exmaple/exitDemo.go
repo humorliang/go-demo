adcfb3da6fbed32a1feb4f38da82fe38b28a7e28
humorliang <humorliang@163.com>
2018-11-29 17:44:11 +0800

    exit

--- src/exmaple/exitDemo.go
+++ src/exmaple/exitDemo.go
@@ -1,34 +1,16 @@
 package main
 
 import (
-	"os"
-	"os/signal"
-	"syscall"
 	"fmt"
+	"os"
 )
 
 /*
-我们希望 Go 能智能的处理 Unix 信号。
-例如，我们希望当服务器接收到一个 SIGTERM 信号时能够自动关机，或者一个命令行工具在接收到一个 SIGINT 信号时停止处理输入信息。
-Go 通过向一个通道发送 os.Signal 值来进行信号通知
+退出
+当使用 os.Exit 时 defer 将不会 执行，所以这里的
 */
 func main() {
-	//信号通道
-	sigs := make(chan os.Signal, 1)
-	done:=make(chan bool,1)
+	defer fmt.Println("!")
 
-	//注册这个通过，接受特定的信号
-	signal.Notify(sigs,syscall.SIGINT,syscall.SIGEMT)
-
-	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
-	}()
-
-	fmt.Println("awaiting signal")
-	<-done
-	fmt.Println("exiting")
-
+	os.Exit(3)
 }
