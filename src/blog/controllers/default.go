19075af7a797f415c156536250683220a77f4995
humorliang <humorliang@163.com>
2018-12-25 17:54:39 +0800

    c

---
+++ src/blog/controllers/default.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+
+//路由控制
+type MainController struct {
+	beego.Controller
+}
+
+//API数据类型
+
+
+//get 请求
+func (c *MainController) Get() {
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "astaxie@gmail.com"
+	//c.TplName = "index.tpl"
+	c.Data[""]
+}
