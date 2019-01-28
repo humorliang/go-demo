f7f8961e7d0ac09ce7b625595c0bee68282fad58
humorliang <humorliang@163.com>
2019-01-28 13:48:22 +0800

    con

--- src/pattern/objpool/main.go
+++ src/pattern/objpool/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"sync"
 	"pattern/objpool/pool"
+	"fmt"
 )
 
 func main() {
-	p := pool.New(10)
-	select {
-	case obj := <-p:
-
+	p := pool.New(3)
+	wait := sync.WaitGroup{}
+	for i := 0; i < 10; i++ {
+		index := i
+		wait.Add(1)
+		go func(pool pool.Pool, ind int) {
+			select {
+			case i := <-*p:
+				i.Do(ind)
+				*p <- i
+			default:
+				fmt.Println("等待对象池对象")
+			}
+			wait.Done()
+		}(*p, index)
 	}
+	wait.Wait()
 }
