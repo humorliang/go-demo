3197f8359bf86d5510a2325c9eacd1cdcdb93b88
humorliang <humorliang@163.com>
2019-02-02 14:02:49 +0800

    code snippet

--- src/codesnippets/snippet47/main.go
+++ src/codesnippets/snippet47/main.go
@@ -17,11 +17,9 @@ type Page struct {
 var templates = template.Must(template.ParseGlob("templates/*"))
 
 func main() {
-
 	p := Page{Content: "content"}
 	err := templates.ExecuteTemplate(os.Stdout, "index.html", p)
 	if err != nil {
 		log.Fatal("Cannot Get View ", err)
 	}
-
 }
