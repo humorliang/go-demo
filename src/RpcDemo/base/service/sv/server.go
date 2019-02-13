3ee349775e52dfd61a98e16399f84faa6cf5f0ea
humorliang <humorliang@163.com>
2019-02-13 10:20:36 +0800

    rpc

---
+++ src/RpcDemo/base/service/sv/server.go
@@ -0,0 +1,29 @@
+package service
+
+import "context"
+
+/*
+编写服务端
+*/
+
+//参数结构体
+type Args struct {
+	A int
+	B int
+}
+
+//响应结构体
+type Reply struct {
+	C int
+}
+
+//定义了一个叫做 Arith 的 service， 并且为它实现了 Mul 方法。
+//服务类型
+type Arith int
+
+// 服务方法
+func (t *Arith) Mul(ctx context.Context, args *Args, reply *Reply) error {
+	reply.C = args.A * args.B
+	return nil
+}
+
