503d967d95d672deea8831cadad1cd031a928bfc
humorliang <humorliang@163.com>
2019-02-22 09:58:29 +0800

    philosorpher

--- src/algorithm/philorsorpher_test.go
+++ src/algorithm/philorsorpher_test.go
@@ -2,22 +2,19 @@ package algorithm
 
 import (
 	"testing"
-	"fmt"
+	"sync"
 )
 
 func TestPhilosopher(t *testing.T) {
 	//初始化
 	fork := NewFork()
-	//var philorsor1 = Philosorpher{name: 0}
-	//var philorsor2 = Philosorpher{name: 1}
-	//var philorsor3 = Philosorpher{name: 2}
-	//var philorsor4 = Philosorpher{name: 3}
-	//var philorsor5 = Philosorpher{name: 4}
-	//
-	//go Run(philorsor1,fork)
-	//go Run(philorsor2,fork)
-	//go Run(philorsor3,fork)
-	//go Run(philorsor4,fork)
-	//go Run(philorsor5,fork)
+
+	var wg = sync.WaitGroup{}
+	for i := 0; i < 5; i++ {
+		var phor = Philosorpher{Name: i, Fr: fork}
+		wg.Add(1)
+		go phor.Run()
+	}
+	wg.Wait()
 
 }
--- src/algorithm/philosorpher.go
+++ src/algorithm/philosorpher.go
@@ -6,6 +6,13 @@ import (
 	"sync"
 )
 
+/*
+问题描述：一圆桌前坐着5位哲学家，两个人中间有一只筷子，桌子中央有面条。
+哲学家思考问题，当饿了的时候拿起左右两只筷子吃饭，必须拿到两只筷子才能吃饭。
+上述问题会产生死锁的情况，当5个哲学家都拿起自己右手边的筷子，准备拿左手边的筷子时产生死锁现象。
+
+2、每个哲学家必须确定自己左右手的筷子都可用的时候，才能同时拿起两只筷子进餐，吃完之后同时放下两只筷子。
+*/
 //定义哲学家
 type Philosorpher struct {
 	Name int
@@ -14,7 +21,7 @@ type Philosorpher struct {
 
 //Fork
 type Fork struct {
-	mu   sync.Mutex
+	mu   sync.Mutex   //锁
 	chop [5]chan bool //筷子
 }
 
@@ -30,6 +37,7 @@ func (this Philosorpher) Thinking() {
 	time.Sleep(1 * time.Second)
 }
 
+//哲学家干的事情
 func (this Philosorpher) Run() {
 	for true {
 		this.Thinking()
@@ -38,6 +46,8 @@ func (this Philosorpher) Run() {
 		this.Fr.PutFork(this)
 	}
 }
+
+//初始化筷子
 func NewFork() Fork {
 	var ch01 = make(chan bool, 1)
 	var ch02 = make(chan bool, 1)
@@ -56,14 +66,28 @@ func NewFork() Fork {
 	}
 }
 
+/*
+人和筷子的位置
+		   0    p0     1
+	p4   		            p1
+
+	4						2
+
+	p3			3			p2
+
+*/
+
 //拿筷子
 func (this Fork) TakeFork(p Philosorpher) {
 	this.mu.Lock()
 	defer this.mu.Unlock()
 	//获取哲学家
 	pId := p.Name
+	//当哲学家拿到一根筷子没有另外一个筷子的时候进行通道阻塞，
 	select {
+	//当满足一根筷子的时候进行另一个筷子判断
 	case <-this.chop[pId]:
+		//当这根筷子满足时吃饭，不满足进行阻塞
 		select {
 		case <-this.chop[(pId+1)%5]:
 		}
@@ -78,6 +102,7 @@ func (this Fork) TakeFork(p Philosorpher) {
 //放筷子
 func (this Fork) PutFork(p Philosorpher) {
 	pId := p.Name
+	//向通道里面进行写  代表放筷子
 	this.chop[pId] <- true
 	this.chop[(pId+1)%5] <- true
 	fmt.Println("放筷子")
